c8d47b8503e969c44269104c73772dee1a4d9b2c
U-WIN-XP\Admin <nietzsche@lysator.liu.se>
2014-09-07 15:53:09 +0200

    Migrate to VS2010.

--- COM/Old_Version/CoClassSyms.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COCLASSSYMS_H_
-#define _COCLASSSYMS_H_
-
-extern BOOL InitProcessTypeLib();
-extern BOOL ExitProcessTypeLib();
-extern void ProcessTypeLib(const char *pszFileName);
-
-#endif
--- COM/Old_Version/Stringify.h
+++
@@ -1,6 +0,0 @@
-#ifndef _STRINGIFY_H_
-#define	_STRINGIFY_H_
-
-void c_stringifyCOMMethod(FUNCDESC* funcDesc, ITypeInfo* pti, char *szBuf, int nMaxLen, BOOL bIDLStyle = TRUE);
-
-#endif
